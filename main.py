774c31c798efd7fc6778a0bdcf25f1c104f02eb1
Daniel-Oliynyk <daniel.oliynyk@gmail.com>
2019-12-29 15:00:53 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import arcade
+
+arcade.open_window(1084, 720, 'The Game')
+arcade.set_background_color(arcade.color.AIR_SUPERIORITY_BLUE)
+arcade.start_render()
+
+arcade.finish_render()
+arcade.run()
