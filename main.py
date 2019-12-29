af8f9b1ec52433ce28605893e6d82e4e1e1d86c8
andrew-trautrim <atrautrim7@gmail.com>
2019-12-29 17:26:01 -0500

    Added player movement

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 import arcade
 import os
 
-SCREEN_WIDTH = 1084
-SCREEN_HEIGHT = 720
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
+PLAYER_SPEED = 5
 test = None
+
+class Player():
+    def __init__(self, position_x, position_y, color):
+        self.position_x = position_x
+        self.position_y = position_y
+        self.change_x = 0
+        self.change_y = 0
+        self.color = color
+    
+    def draw(self):
+        arcade.draw_circle_filled(self.position_x, self.position_y, 32, self.color)
+
+    def update(self):
+        self.position_x += self.change_x
+        self.position_y += self.change_y
 
 class SunshineSuperman(arcade.Window):
 
     def __init__(self, width, height):
         super().__init__(width, height)
         arcade.set_background_color(arcade.color.BABY_BLUE_EYES)
+        self.player = Player(64, 64, arcade.color.WHITE)
 
     def setup(self):
         global test
@@ -18,10 +35,20 @@ class SunshineSuperman(arcade.Window):
 
     def on_draw(self):
         arcade.start_render()
-        arcade.draw_texture_rectangle(400, 400, 256, 256, test)
+        self.player.draw()
 
     def update(self, delta_time):
-        pass
+        self.player.update()
+
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.LEFT:
+            self.player.change_x = -PLAYER_SPEED
+        elif key == arcade.key.RIGHT:
+            self.player.change_x = PLAYER_SPEED
+
+    def on_key_release(self, key, modifiers):
+        if key == arcade.key.LEFT or key == arcade.key.RIGHT:
+            self.player.change_x = 0
 
 def main():
     game = SunshineSuperman(SCREEN_WIDTH, SCREEN_HEIGHT)
