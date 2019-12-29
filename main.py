8c77986fdda0c22312ae27ce92d519d6afa62c36
Daniel-Oliynyk <daniel.oliynyk@gmail.com>
2019-12-29 16:45:53 -0500

    Added importing images

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import arcade
+import os
 
 SCREEN_WIDTH = 1084
 SCREEN_HEIGHT = 720
-
+test = None
 
 class SunshineSuperman(arcade.Window):
 
@@ -11,10 +12,13 @@ class SunshineSuperman(arcade.Window):
         arcade.set_background_color(arcade.color.BABY_BLUE_EYES)
 
     def setup(self):
-        pass
+        global test
+        filename = os.getcwd() + '/assets/grass.tif'
+        test = arcade.load_texture(filename, scale=1)
 
     def on_draw(self):
         arcade.start_render()
+        arcade.draw_texture_rectangle(400, 400, 256, 256, test)
 
     def update(self, delta_time):
         pass
